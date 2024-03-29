551e82b6207d2bf5dede59e18b7099d8542b4d8d
Tarun-Nagesh <btnagesh12@gmail.com>
2024-03-29 16:55:29 -0400

    Update FishSurgeon.sql

--- FishSurgeon.sql
+++ FishSurgeon.sql
@@ -9,12 +9,32 @@ CREATE TABLE Surgeons (
     LastName TEXT,
     Hours INT
 );
+CREATE TABLE Managers (
+    ManagerID INT NOT NULL PRIMARY KEY,
+    FirstName TEXT,
+    MiddleInitial TEXT,
+    LastName TEXT,
+    Budget INT
+)
+CREATE TABLE Tanks (
+    TankID INT NOT NULL PRIMARY KEY,
+    ManagerID INT NOT NULL,
+    Status TEXT,
+    CONSTRAINT fk01 FOREIGN KEY (ManagerID)
+        REFERENCES Managers(ManagerID)
+);
 CREATE TABLE Fish(
     FishID INT NOT NULL PRIMARY KEY,
+    TankID INT NOT NULL,
+    ManagerID INT NOT NULL,
     Notes LONGTEXT,
     Sex TEXT,
     Species TEXT,
     Status TEXT,
+    CONSTRAINT fk01 FOREIGN KEY (TankID)
+        REFERENCES Tanks(TankID),
+    CONSTRAINT fk02 FOREIGN KEY (ManagerID)
+        REFERENCES Managers(ManagerID)
 );
 CREATE TABLE Procedures (
     EmpID INT NOT NULL,
@@ -32,7 +52,7 @@ CREATE TABLE FishProcDate (
     FishID INT NOT NULL,
     ProcDate DATETIME NOT NULL,
     PRIMARY KEY (FishId, ProcDate),
-    CONSTRAINT fk03 FOREIGN KEY (FishID)
+    CONSTRAINT fk01 FOREIGN KEY (FishID)
         REFERENCES Fish(FishID)
 );
 CREATE TABLE Prescriptions (
@@ -41,7 +61,7 @@ CREATE TABLE Prescriptions (
     Medicine TEXT,
     Dosage FLOAT,
     PRIMARY KEY (ProcID, MedID),
-    CONSTRAINT fk04 FOREIGN KEY (ProcID)
+    CONSTRAINT fk01 FOREIGN KEY (ProcID)
         REFERENCES Procedures(ProcID)
 );
 CREATE TABLE Tools (
@@ -49,24 +69,25 @@ CREATE TABLE Tools (
     ProcID INT NOT NULL,
     Type TEXT,
     Status TEXT,
-    CONSTRAINT fk05 FOREIGN KEY (ProcID)
+    CONSTRAINT fk01 FOREIGN KEY (ProcID)
         REFERENCES Procedures(ProcID)
 );
 CREATE TABLE Employees (
     EmpID INT NOT NULL PRIMARY KEY,
+    ManagerID INT NOT NULL,
     FirstName TEXT,
     MiddleInitial TEXT,
     LastName TEXT,
-    Role TEXT
+    Role TEXT,
+    CONSTRAINT fk01 FOREIGN KEY (ManagerID)
+        REFERENCES Managers(ManagerID)
 );
 CREATE TABLE Finances (
     TransactionID INT NOT NULL PRIMARY KEY,
+    ManagerID INT NOT NULL,
     Recievables INT,
     Payables INT,
-    DateSent DATETIME
+    DateSent DATETIME,
+    CONSTRAINT fk01 FOREIGN KEY (ManagerID)
+        REFERENCES Managers(ManagerID)
 );
-CREATE TABLE Tanks (
-    TankID INT NOT NULL PRIMARY KEY,
-    Status TEXT,
-
-)
