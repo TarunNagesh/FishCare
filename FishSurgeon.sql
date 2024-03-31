ee8646f06010d157aad625bcf6f4982412c890b8
Tarun-Nagesh <btnagesh12@gmail.com>
2024-03-31 15:05:34 -0400

    Update FishSurgeon.sql

--- FishSurgeon.sql
+++ FishSurgeon.sql
@@ -15,9 +15,10 @@ CREATE TABLE Employees (
         ON UPDATE CASCADE ON DELETE RESTRICT
 
 );
-CREATE TABLE Keeper (
-    KeeperID INT NOT NULL PRIMARY KEY,
-    CONSTRAINT fk00 FOREIGN KEY (KeeperID)
+CREATE TABLE Managers (
+    ManagerID INT NOT NULL PRIMARY KEY,
+    Budget INT,
+    CONSTRAINT fk02 FOREIGN KEY (ManagerID)
         REFERENCES Employees(EmpID)
         ON UPDATE CASCADE ON DELETE RESTRICT
 );
@@ -28,10 +29,9 @@ CREATE TABLE Surgeons (
         REFERENCES Employees(EmpID)
         ON UPDATE CASCADE ON DELETE RESTRICT
 );
-CREATE TABLE Managers (
-    ManagerID INT NOT NULL PRIMARY KEY,
-    Budget INT,
-    CONSTRAINT fk02 FOREIGN KEY (ManagerID)
+CREATE TABLE Keeper (
+    KeeperID INT NOT NULL PRIMARY KEY,
+    CONSTRAINT fk00 FOREIGN KEY (KeeperID)
         REFERENCES Employees(EmpID)
         ON UPDATE CASCADE ON DELETE RESTRICT
 );
@@ -61,25 +61,24 @@ CREATE TABLE Plans (
 CREATE TABLE Tanks (
     TankID INT NOT NULL PRIMARY KEY,
     ManagedBy INT NOT NULL,
-    OverseedBy INT NOT NULL,
+    OverseenBy INT NOT NULL,
     Temp INT NOT NULL,
     TimeCleaned DATETIME,
     WaterType TEXT,
-    PHlevel INT,
+    PHlevel FLOAT,
     Food TEXT,
     TimeFed DATETIME,
     Status TEXT,
     CONSTRAINT fk09 FOREIGN KEY (ManagedBy)
         REFERENCES Managers(ManagerID)
         ON UPDATE CASCADE ON DELETE RESTRICT,
-    CONSTRAINT fk10 FOREIGN KEY (OverseedBy)
+    CONSTRAINT fk10 FOREIGN KEY (OverseenBy)
         REFERENCES Ichthyologist(IchID)
         ON UPDATE CASCADE ON DELETE RESTRICT
 );
 CREATE TABLE Fish(
     FishID INT NOT NULL PRIMARY KEY,
     HousedIn INT NOT NULL,
-    ManagedBy INT NOT NULL,
     KeptBy INT NOT NULL,
     Notes LONGTEXT,
     Sex TEXT,
@@ -87,9 +86,6 @@ CREATE TABLE Fish(
     Status TEXT,
     CONSTRAINT fk11 FOREIGN KEY (HousedIn)
         REFERENCES Tanks(TankID)
-        ON UPDATE CASCADE ON DELETE RESTRICT,
-    CONSTRAINT fk12 FOREIGN KEY (ManagedBy)
-        REFERENCES Managers(ManagerID)
         ON UPDATE CASCADE ON DELETE RESTRICT,
     CONSTRAINT fk13 FOREIGN KEY (KeptBy)
         REFERENCES Keeper(KeeperID)
@@ -148,6 +144,7 @@ CREATE TABLE Finances (
         ON UPDATE CASCADE ON DELETE RESTRICT
 );
 CREATE TABLE Reports (
+    ReportID INT NOT NULL PRIMARY KEY,
     FishID INT NOT NULL,
     MadeBy INT NOT NULL,
     SentTo INT NOT NULL,
@@ -164,8 +161,69 @@ CREATE TABLE Reports (
         ON UPDATE CASCADE ON DELETE RESTRICT
 );
 
-INSERT INTO Surgeons(Certification, SurgeonID)
-VALUES('PhD in Marine Biology at UCLA', 1);
-
-INSERT INTO Surgeons(Certification, SurgeonID)
-VALUES('PhD in Vetrinary Studies at Northeastern', 2);
+INSERT INTO Employees
+    VALUES (0001, 0001, 'John', 'P', 'Park', 40, '123 Antwerp St'),
+           (0002, 0002, 'Park', 'A', 'Nontenot', 80, '2 Merserve Terr'),
+           (0003, 0001, 'Marissa', 'J', 'Kent', 20, '300 Huntington Ave'),
+           (0004, 0001, 'Projes', 'U', 'Crane', 25, '90 Adams St'),
+           (0005, 0002, 'Corg', 'I', 'Gatsby', 45, '21 Atlantis C'),
+           (0006, 0002, 'Craig', 'H', 'Ate', 44, '20 Atlantis C'),
+           (0007, 0001, 'Doc', 'T', 'Erfish', 34, '10 Salt Lake Blvd'),
+           (0008, 0001, 'Stur', 'G', 'Eon', 18, '1 Street Ave');
+INSERT INTO Managers
+    VALUES (0001, 8000),
+           (0002, 500);
+INSERT INTO Surgeons
+    VALUES (0003, 'PHD in Marine Biology Operations @ UCLA'),
+           (0004, 'Masters in Veterinary Surgery @ SMU');
+INSERT INTO Keeper
+    VALUES (0005),
+           (0006);
+INSERT INTO Ichthyologist
+    VALUES (0007, 'PHD in Marine Biology Observation @ Yale'),
+           (0008, 'Post Doc in Aquarium Management @ SASE');
+INSERT INTO Plans
+    VALUES (1111, 0007, 0001, 'Breeding',
+            'Sturgeon population is declining to point of endangerment:
+            need to invest in a breeding tank.',
+            'Approved', 2000),
+        (2222, 0008, 0001, 'Expansion',
+            'Fish in tank seem to be cramped: could benefit from separating entities',
+            'Denied', 10);
+INSERT INTO Tanks
+    VALUES (1000, 0002, 0007, 79, CURRENT_TIMESTAMP, 'Fresh Water',
+            7.3, 'PetCo', CURRENT_TIMESTAMP, 'Active'),
+        (2000, 0002, 0008, 75, CURRENT_TIMESTAMP, 'Salt Water',
+         7.1, 'PetSmart', CURRENT_TIMESTAMP, 'Active'),
+        (3000, 0002, 0007, 78, CURRENT_TIMESTAMP, 'Fresh Water',
+         6.9, 'NontenotHungry', CURRENT_TIMESTAMP, 'Inactive');
+INSERT INTO Fish
+    VALUES (1, 1000, 0005, 'Lorem Ipsum Switch', 'F', 'Sturgeon', 'Alive'),
+           (2, 1000, 0005, 'Lorem Ipsum Switch', 'M', 'Sturgeon', 'Alive'),
+           (3, 2000, 0006, 'Lorem Ipsum Switch', 'M', 'Tuna', 'Alive'),
+           (4, 2000, 0006, 'Lorem Ipsum Switch', 'M', 'Fluke', 'Dead');
+INSERT INTO Procedures
+    VALUES (0003, 9999, 1, 'Scale Removal', 'Invasive', 'Success'),
+           (0003, 9998, 2, 'Wound Closure', 'Invasive', 'Success'),
+           (0004, 9997, 4, 'Eye Check-Up', 'Non-invasive', 'Failure'),
+           (0004, 9996, 4, 'Resuscitation', 'Invasive', 'Failure');
+INSERT INTO FishProcDate
+    VALUES (1, CURRENT_TIMESTAMP),
+           (2, CURRENT_TIMESTAMP),
+           (4, CURRENT_TIMESTAMP),
+           (4, CURRENT_TIMESTAMP);
+INSERT INTO Prescriptions
+    VALUES (9999, 82792, 'Advil', 1.5),
+           (9997, 92973, 'CPR', 1.0),
+           (9996, 93898, 'Funeral', 1.0);
+INSERT INTO Tools
+    VALUES (10001, 9999, 'Scalpel', 'Clean'),
+           (10002, 9999, 'Sutures', 'Used'),
+           (10003, 9998, 'Sutures', 'Used');
+INSERT INTO Finances
+    VALUES (20001, 0001, 8000, 5000, CURRENT_TIMESTAMP),
+           (20002, 0002, 0, 10000, CURRENT_TIMESTAMP);
+INSERT INTO Reports
+    VALUES (101, 1, 0005, 0007, 'Impaled Scale', 'Lorem Ipsum Switch'),
+           (102, 2, 0005, 0007, 'Bite Wound', 'Lorem Ipsum Switch'),
+           (103, 4, 0006, 0008, 'Lazy Eye', 'Lorem Ipsum Switch');
