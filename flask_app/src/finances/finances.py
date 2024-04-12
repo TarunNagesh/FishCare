f0448266c0e39fc73f24d8122afa8e13393234b9
fifilala097 <sophia.boyajian@gmail.com>
2024-04-12 18:53:53 -0400

    Added a GET command for finances

--- flask_app/src/finances/finances.py
+++ flask_app/src/finances/finances.py
@@ -5,6 +5,34 @@ from src import db
 
 finances = Blueprint('finances', __name__)
 
+# Get all the plans from the database
+@finances.route('/plans', methods=['GET'])
+def finances():
+    # get a cursor object from the database
+    cursor = db.get_db().cursor()
+
+    # use cursor to query the database for a list of plans
+    # cursor.execute('SELECT id, plans_code, plans_name, list_price FROM plans')
+    cursor.execute('')
+
+    # grab the column headers from the returned data
+    column_headers = [x[0] for x in cursor.description]
+
+    # create an empty dictionary object to use in 
+    # putting column headers together with data
+    json_data = []
+
+    # fetch all the data from the cursor
+    theData = cursor.fetchall()
+
+    # for each of the rows, zip the data elements together with
+    # the column headers. 
+    for row in theData:
+        json_data.append(dict(zip(column_headers, row)))
+
+    return jsonify(json_data)
+
+
 @finances.route('/finances', methods=['POST'])
 def add_new_plans():
     
