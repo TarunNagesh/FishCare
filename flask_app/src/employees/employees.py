63f929c2ed5033191b0460cdb232c0663850fe04
fin li <lifin818@gmail.com>
2024-04-15 15:42:37 -0400

    updated employee

--- flask_app/src/employees/employees.py
+++ flask_app/src/employees/employees.py
@@ -48,3 +48,89 @@ def get_manager (id):
         json_data.append(dict(zip(column_headers, row)))
     return jsonify(json_data)
 
+# Add a new employee
+@employees.route('/employees', methods=['POST'])
+def add_new_employee():
+    
+    # collecting data from the request object 
+    the_data = request.json
+    current_app.logger.info(the_data)
+
+    #extracting the variable
+    employee_id = the_data['empid']
+    manager_id = the_data['managerid']
+    first = the_data['firstname']
+    mi = the_data['middleinitial']
+    last = the_data['lastname']
+    hours = the_data['hours']
+    address = the_data['address']
+
+    # Constructing the query
+    query = 'INSERT INTO prescriptions (empid, managerid, firstname, middleinitial, lastname, hours, address) VALUES ("'
+    query += employee_id + '", "'
+    query += manager_id + '", "'
+    query += first + '", "'
+    query += mi + '", "'
+    query += last + '", "'
+    query += hours + '", "'
+    query += address + ')'
+    current_app.logger.info(query)
+
+    # executing and committing the insert statement 
+    cursor = db.get_db().cursor()
+    cursor.execute(query)
+    db.get_db().commit()
+    
+    return 'Success!'
+
+# Update the details of an employee
+@employees.route('/employees', methods=['PUT'])
+def update_employee():
+    the_data = request.json
+    # current_app.logger.info(cust_info)
+    employee_id = the_data['empid']
+    manager_id = the_data['managerid']
+    first = the_data['firstname']
+    mi = the_data['middleinitial']
+    last = the_data['lastname']
+    hours = the_data['hours']
+    address = the_data['address']
+
+    query = 'UPDATE prescriptions SET managerid = %s, firstname = %s, middleinitial = %s, lastname = %s, hours = %s, address = %s WHERE empid = %s'
+    data = (manager_id, first, mi, last, hours, address, employee_id)
+
+    cursor = db.get_db().cursor()
+    r = cursor.execute(query, data)
+    db.get_db().commit()
+    return 'Employee updated!'
+
+# Delete employees where hours = 0
+@employees.route('/employees', methods = ['DELETE'])
+def delete_employee_no_hours():
+    the_data = request.json
+    current_app.logger.info(the_data) 
+
+    query = 'DELETE FROM employees WHERE hours == 0'
+    current_app.logger.info(query)
+
+    # executing and committing the insert statement 
+    cursor = db.get_db().cursor()
+    cursor.execute(query)
+    db.get_db().commit()
+    return 'Employee fired!'
+
+# Delete employee given id
+@employees.route('/employees/<EmpID>', methods = ['DELETE'])
+def delete_employee():
+    the_data = request.json
+    current_app.logger.info(the_data) 
+    id = the_data['EmpId']
+
+    query = 'DELETE FROM employees WHERE EmpId == ' + str(id)
+    current_app.logger.info(query)
+
+    # executing and committing the insert statement 
+    cursor = db.get_db().cursor()
+    cursor.execute(query)
+    db.get_db().commit()
+    return 'Employee fired!'
