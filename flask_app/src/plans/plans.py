5e3be0a70447dbbe7f81602d2195b4d5727d6515
Tarun-Nagesh <btnagesh12@gmail.com>
2024-04-15 15:44:42 -0400

    FIXED CAPITILIZATION IN PLANS

--- flask_app/src/plans/plans.py
+++ flask_app/src/plans/plans.py
@@ -9,10 +9,10 @@ plans = Blueprint('plans', __name__)
 @plans.route('/plans', methods=['GET'])
 def plans():
     query = '''
-        SELECT DISTINCT Type AS Label
+        SELECT DISTINCT type AS Label
         FROM plans
-        WHERE Type IS NOT NULL
-        ORDER BY Type
+        WHERE type IS NOT NULL
+        ORDER BY type
     '''
 
     cursor = db.get_db().cursor()
@@ -31,7 +31,7 @@ def plans():
 @plans.route('/plans/<id>', methods=['GET'])
 def get_plan_detail (id):
 
-    query = 'SELECT PlanID, MadeBy, ApprovedBy, Type, Details, Status, Cost FROM plans WHERE PlanID = ' + str(id)
+    query = 'SELECT planID, madeBy, approvedBy, type, details, status, cost FROM plans WHERE planID = ' + str(id)
     current_app.logger.info(query)
 
     cursor = db.get_db().cursor()
@@ -52,16 +52,16 @@ def add_new_plan():
     current_app.logger.info(the_data)
 
     #extracting the variable
-    id = the_data['PlanId']
-    madeBy = the_data['MadeBy']
-    approvedBy = the_data['ApprovedBy']
-    type = the_data['Type']
-    details = the_data['Details']
-    status = the_data['Status']
-    cost = the_data['Cost']
+    id = the_data['planId']
+    madeBy = the_data['madeBy']
+    approvedBy = the_data['approvedBy']
+    type = the_data['type']
+    details = the_data['details']
+    status = the_data['status']
+    cost = the_data['cost']
 
     # Constructing the query
-    query = 'insert into plans (PlanID, MadeBy, ApprovedBy, Type, Details, Status, Cost) values ("'
+    query = 'insert into plans (planID, madeBy, approvedBy, type, details, status, cost) values ("'
     query += id + '", "'
     query += madeBy + '", "'
     query += approvedBy + '", "'
@@ -87,17 +87,17 @@ def update_plan():
     current_app.logger.info(the_data)
 
     #extracting the variable
-    id = the_data['PlanId']
-    madeBy = the_data['MadeBy']
-    approvedBy = the_data['ApprovedBy']
-    type = the_data['Type']
-    details = the_data['Details']
-    status = the_data['Status']
-    cost = the_data['Cost']
+    id = the_data['planId']
+    madeBy = the_data['madeBy']
+    approvedBy = the_data['approvedBy']
+    type = the_data['type']
+    details = the_data['details']
+    status = the_data['status']
+    cost = the_data['cost']
 
 
     # Constructing the query
-    query = 'UPDATE plans SET MadeBy = %s, ApprovedBy = %s, Type = %s, Details = %s, Status = %s, Cost = %s, WHERE PlanID = %s'
+    query = 'UPDATE plans SET madeBy = %s, approvedBy = %s, type = %s, details = %s, status = %s, cost = %s, WHERE planID = %s'
     current_app.logger.info(query)
     
     
@@ -114,9 +114,9 @@ def delete_plan_id():
     the_data = request.json
     current_app.logger.info(the_data) 
 
-    id = the_data['PlanID']
+    id = the_data['planID']
 
-    query = 'DELETE FROM plans WHERE PlanID = ' + str(id)
+    query = 'DELETE FROM plans WHERE planID = ' + str(id)
     current_app.logger.info(query)
 
     # executing and committing the insert statement 
@@ -131,7 +131,7 @@ def delete_denied_plans():
     the_data = request.json
     current_app.logger.info(the_data) 
 
-    query = 'DELETE FROM plans WHERE Status = "Denied"'
+    query = 'DELETE FROM plans WHERE status = "Denied"'
     current_app.logger.info(query)
 
     # executing and committing the insert statement 
